f9ba1a6d9b6de2a782077a5b8f2c0271f1a1daf0
Gabiel679 <gabriel.juan@e-campus.uab.cat>
2021-05-24 21:20:45 +0200

    h

--- my_labeling.py
+++ my_labeling.py
@@ -12,10 +12,12 @@ if __name__ == '__main__':
 
     #Load all the images and GT
     train_imgs, train_class_labels, train_color_labels, \
-    test_imgs, test_class_labels, test_color_labels = read_dataset(ROOT_FOLDER='./images/', gt_json='./images/gt.json')
+    test_imgs, test_class_labels, test_color_labels = read_dataset(ROOT_FOLDER='./images/', gt_json='./test/gt.json')
 
     #List with all the existant classes
     classes = list(set(list(train_class_labels) + list(test_class_labels)))
+    e = Kmeans_stadistics(Kmeans, 20)
+
 
 
 
@@ -28,7 +30,9 @@ def Kmean_stadistics(Kmeans, Kmax):
             Kmeans.fit()
             Kmeans.whitinClassDistance()
     
-    return visualize_k_means(Kmeans, test_imgs.shape())
+    revisualize_k_means(Kmeans, test_imgs.shape())
+
+
 
 
 
