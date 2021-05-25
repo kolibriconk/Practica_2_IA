b880756e62f2e7bdd300d4b5df1a812eebca8590
es2021uab-1565479 <joseantonio.ramosa@e-campus.uab.cat>
2021-05-26 00:49:05 +0200

    Added some changes

--- my_labeling.py
+++ my_labeling.py
@@ -1,24 +1,24 @@
 __authors__ = ['1571610', '1562750', '1565479']
 __group__ = 'DM.18'
 
+import time
+
 import numpy as np
 import Kmeans
 import KNN
-from utils_data import read_dataset, visualize_k_means, visualize_retrieval
-import matplotlib.pyplot as plt
+from utils_data import read_dataset, visualize_retrieval
 import cv2
-import os
 
 
 def train_program():
     # Train classes
-    train_classes_num = round(0.20 * train_imgs.shape[0])  # Limit to 20% of images
+    train_classes_num = round(train_imgs.shape[0])  # Limit to 20% of images
     knn = KNN.KNN(train_imgs[:train_classes_num],
                   train_class_labels[:train_classes_num])
 
     # Test classes
-    test_classes_num = round(0.80 * test_imgs.shape[0])  # Limit to 70% of test classes
-    class_labels = knn.predict(test_imgs[:test_classes_num], 5)
+    test_classes_num = round(test_imgs.shape[0])  # Limit to 70% of test classes
+    class_labels = knn.predict(test_imgs[:test_classes_num], 6)
 
     return train_classes_num, test_classes_num, class_labels
 
@@ -33,27 +33,33 @@ def retrieval_by_shape(images, labels, question):
     return np.array(images_matching)
 
 
-def Kmean_stadistics(Kmeans, Kmax):
-    Kmeans.__init__(test_imgs)
-    for k in Kmax:
-        if k > 2:
-            Kmeans.fit()
-            Kmeans.whitinClassDistance()
-    
-    visualize_k_means(Kmeans, test_imgs.shape())
+def get_shape_accuracy(labels, gt_labels):
+    total = len(labels)
+    correct = np.sum(labels == gt_labels)
+
+    return correct/total*100
 
 
 if __name__ == '__main__':
+
     # Load all the images and GT
     train_imgs, train_class_labels, train_color_labels, \
     test_imgs, test_class_labels, test_color_labels = read_dataset(ROOT_FOLDER='./images/', gt_json='./test/gt.json')
 
     # List with all the existant classes
     classes = list(set(list(train_class_labels) + list(test_class_labels)))
-
+    startTime = time.time()
     train_class_num, test_classes_num, class_labels = train_program()
+    endTime = time.time()
+    print("Training class time : {:.2f}".format(endTime - startTime))
 
     results = retrieval_by_shape(test_imgs[:test_classes_num], class_labels, "Jeans")
-    visualize_retrieval(results, 10)
+
+    print("Retrieval by shape completed time was: {}".format(endTime-startTime))
+
+    percent = get_shape_accuracy(class_labels, test_class_labels[:test_classes_num])
+    print("The % of the shape accuracy is {}%".format(round(percent, 2)))
+
+    visualize_retrieval(results, 12)
 
 
